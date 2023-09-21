cbec96e2b0e88623241f014ab61153369a30c6ac
mariobd38 <mariobd38@gmail.com>
2023-09-21 14:57:28 -0700

    Adding Stringwiz to Git

---
+++ src/main/java/com/string/project/controllers/UsersController.java
@@ -0,0 +1,17 @@
+package com.string.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersController {
+    @GetMapping("/signup")
+    public String getSignUpPage() {
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+}
